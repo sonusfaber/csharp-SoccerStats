7ca5352cd4a913d9d7bd1f2b108646cff1b2b334
trey causey <tcausey808@gmail.com>
2017-07-15 12:53:21 -0400

    Initial Commit 7/15

---
+++ SoccerStats/SoccerStats/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace SoccerStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+
+            Console.WriteLine("");
+
+
+        }
+    }
+}
